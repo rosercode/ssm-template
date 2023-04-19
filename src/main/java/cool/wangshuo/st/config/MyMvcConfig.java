e866ddffac18b6d734cf25ced5721e6e314ae6c6
rosercode <rosercode@gmail.com>
2023-04-19 21:47:57 +0800

    feat Add Cors Config

--- src/main/java/cool/wangshuo/st/config/MyMvcConfig.java
+++ src/main/java/cool/wangshuo/st/config/MyMvcConfig.java
@@ -1,7 +1,9 @@
 package cool.wangshuo.st.config;
 
+import cool.wangshuo.st.component.interceptor.AuthInterceptor;
 import cool.wangshuo.st.component.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
-    // ......
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor())
@@ -28,6 +30,23 @@ public class MyMvcConfig implements WebMvcConfigurer {
 //                        "/**/*.png"
 //                );
     }
+
+    /**
+     * 跨域相关配置
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //放行哪些原始域
+                .allowedOriginPatterns("*")
+                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
